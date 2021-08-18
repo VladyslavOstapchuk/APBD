e6cd933d1ed61a4e36c51219567bc2ce385913aa
VladyslavOstapchuk <57323288+VladyslavOstapchuk@users.noreply.github.com>
2021-08-18 17:46:23 +0200

    Add files via upload

---
+++ cwiczenia_1-s18423/c1/Program.cs
@@ -0,0 +1,38 @@
+﻿//import
+using System;
+using System.Collections.Generic;
+
+namespace c1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //addStudent() - camelCase
+            //AddStudent - PascalCase
+
+            //Write smth in console
+            Console.WriteLine("Some text");
+
+            //Ctrl + Space - list props of the class
+            Student student1 = new Student { 
+            FirstName = "Vladyslav",
+            Surname = "Ostapchuk",
+            SNumber = 18423
+            };
+
+            //String interpolation
+            Console.WriteLine($"Student name: {student1.FirstName}\nStudent surname: {student1.Surname}");
+
+            //
+            var tmp = 3 + 3;
+            Console.WriteLine(tmp);
+
+            var list = new List<string> {"ala","ma","kota"};
+            
+            foreach (var wrt in list) {
+                Console.WriteLine(wrt);
+            }
+        }
+    }
+}
---
+++ cwiczenia_1-s18423/c1/Student.cs
@@ -0,0 +1,29 @@
+﻿//Basic usings
+//Remove if are not nessesary
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c1
+{
+    class Student
+    {
+        //cw - Console.WriteLine()
+        //prop - Property
+
+        //public properties
+        public string FirstName { get; set; }
+        public string Surname{ get; set; }
+
+        //private properties start with _
+        private int _sNumber;
+
+        public int SNumber
+        {
+            get { return _sNumber; }
+            set { _sNumber = value; }
+        }
+    }
+}
